230ceafcb16337a80d83ac89c70e82a6d52d8d20
cjheppell <chris.john.heppell@gmail.com>
2020-10-16 13:43:53 +0100

    add service config and parser

---
+++ config/parser.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func Parse(configPath string) ([]Service, error) {
+	absConfigPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = os.Stat(absConfigPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("config file '%s' does not exist", configPath)
+		}
+		return nil, err
+	}
+
+	yamlFile, err := ioutil.ReadFile(absConfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var serviceConfigs []Service
+	err = yaml.Unmarshal(yamlFile, serviceConfigs)
+	if err != nil {
+		return nil, err
+	}
+
+	return serviceConfigs, nil
+}
---
+++ config/service.go
@@ -0,0 +1,8 @@
+package config
+
+type Service struct {
+	Name           string `yaml:"name"`
+	DeploymentName string `yaml:"deploymentName"`
+	Namespace      string `yaml:"namespace"`
+	ApiPath        string `yaml:"apiPath"`
+}
